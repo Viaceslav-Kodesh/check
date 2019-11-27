8f26a4da43d3babc862c9c8c6fc22f5593859b11
Viaceslav-Kodesh <54391135+Viaceslav-Kodesh@users.noreply.github.com>
2019-11-27 10:30:24 +0200

    Update vks-test-openrc.sh

--- vks-test-openrc.sh
+++ vks-test-openrc.sh
@@ -26,8 +26,6 @@ unset OS_TENANT_NAME
 # performing the action as the **user**.
 export OS_USERNAME="vks-test"
 # With Keystone you pass the keystone password.
-echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
-read -sr OS_PASSWORD_INPUT
 export OS_PASSWORD=Yz6PwpmJK4xGlgpgbf5K
 # If your configuration has multiple regions, we set that information here.
 # OS_REGION_NAME is optional and only valid in certain environments.
