591d3a312e328ab09ff87b65ed385a9fc2db8920
Denis <denis@plerion.io>
2019-12-05 14:57:02 +0200

    fixed

--- check_cluster_creation.py
+++ check_cluster_creation.py
@@ -5,22 +5,26 @@ import sys
 
 count = 0
 while(count < 10):
-        os.system('openstack coe cluster list --format json >result.json')
+        os.system('openstack coe cluster show $OS_CLUSTER_NAME -c status --format json >result.json')
         filename = 'result.json'
         with open('result.json') as json_file:
             data = json.load(json_file)
-            for p in data:
-                print(p['status'])
-            if p['status'] == "CREATE_COMPLETE":
-                                              count=10
-                                              sys.exit(0)
-            if p['status'] == "CREATE_FAILED":
-                                              count=10
+
+            if data['status'] == "CREATE_COMPLETE":
+                count=10
+                sys.exit(0)
+            if data['status'] == "CREATE_FAILED":
+                count=10
             else:
                 time.sleep(60)
                 count += 1
 
 
-if p['status'] == "CREATE_FAILED" and count == 10:
-                                                 os.system('openstack coe cluster delete vks-test')
-                                                 sys.exit(1)
+if data['status'] == "CREATE_FAILED" and count == 10:
+    os.system('openstack coe cluster show $OS_CLUSTER_NAME -c status_reason -c faults --format json >err.json')
+    filename = 'err.json'
+    with open('err.json') as json_file:
+        err = json.load(json_file)
+    print err
+    os.system('openstack coe cluster delete $OS_CLUSTER_NAME')
+    sys.exit(1)
--- check_pods.py
+++ check_pods.py
@@ -5,12 +5,33 @@ from kubernetes.client import CoreV1Api
 import os
 # Configs can be set in Configuration class directly or using helper utility 1
 
-config.load_kube_config()
+
+if os.path.isfile('./config'):
+    print ("Kubeconfig exist")
+else:
+    print ("Kubeconfig not exist")
+    os.system('openstack coe cluster delete $OS_CLUSTER_NAME')
+    sys.exit(1)
+
+array = []
+
+try:
+   config.load_kube_config()
+except:
+    print("Could not load kube config")
+    os.system('openstack coe cluster delete $OS_CLUSTER_NAME')
+    sys.exit(1)
+
 v1 = client.CoreV1Api()
-ret = v1.list_pod_for_all_namespaces(watch=False)
+try:
+   ret = v1.list_pod_for_all_namespaces(watch=False)
+except:
+    print("Could not connect to cluster or something wrong with config")
+    os.system('openstack coe cluster delete $OS_CLUSTER_NAME')
+    sys.exit(1)
+
 for i in ret.items:
    if i.status.phase != "Running":
-       array = []
        array.append(i)
 
 count = 0
@@ -26,7 +47,8 @@ while(count < 10):
 
     if len(array) == 0:
         count=10
-        os.system('openstack coe cluster delete vks-test')
+        os.system('kubectl get po --all-namespaces')
+        os.system('openstack coe cluster delete $OS_CLUSTER_NAME')
         sys.exit(0)
     else:
         time.sleep(10)
@@ -34,6 +56,6 @@ while(count < 10):
 
 if len(array) != 0 and count == 10:
     for i in array:
-        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.status.phase))
-        os.system('openstack coe cluster delete vks-test')
+        print("%s\t%s\t%s" % (i.metadata.name, i.metadata.namespace, i.status.phase))
+        os.system('openstack coe cluster delete $OS_CLUSTER_NAME')
         sys.exit(1)
